d0094014a681a8fbed01cc26b8ac5c642066cadf
ryuseiarai <arai.ryusei.jp@gmail.com>
2024-02-17 16:37:19 +0000

    add terrafrom dir

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.92"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "backstage" {
+  name     = "backstage-rg"
+  location = "japaneast"
+}
